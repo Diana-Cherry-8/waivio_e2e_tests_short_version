a4ff710f2bacc9af604e8ec3897a4d19a7c1411e
Diana <dianache@wizardsdev.com>
2020-12-09 11:28:14 +0200

    General edits, delete unused code

--- src/main/java/com/wizardsdev/PageObjects/FacebookSignPage.java
+++ src/main/java/com/wizardsdev/PageObjects/FacebookSignPage.java
@@ -12,7 +12,6 @@ public class FacebookSignPage extends Page {
   private static final By LOGIN_INPUT_LOCATOR = By.cssSelector("#email");
   private static final By PASSWORD_INPUT_LOCATOR = By.cssSelector("#pass");
   private static final By LOGIN_BUTTON_LOCATOR = By.cssSelector("#u_0_0");
-  private static final By OK_BUTTON_LOCATOR = By.cssSelector(".selected");
 
   public FacebookSignPage() {
     switchTo().window(1);
@@ -33,12 +32,6 @@ public class FacebookSignPage extends Page {
     $(LOGIN_BUTTON_LOCATOR).click();
     switchTo().window(0);
   }
-
-//  @Step
-//  public void clickOnOk() {
-//    $(OK_BUTTON_LOCATOR).shouldBe(Condition.visible).click();
-//    switchTo().window(0);
-//  }
 
   @Override
   protected void init() {
