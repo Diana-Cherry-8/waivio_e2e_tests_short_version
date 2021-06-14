2434dfeb63ef7e5d412c0eb77840c33f2e47c09d
Diana <dianache@wizardsdev.com>
2021-06-14 12:57:13 +0300

    fix languages test

--- src/main/java/com/wizardsdev/PageObjects/FeedPage.java
+++ src/main/java/com/wizardsdev/PageObjects/FeedPage.java
@@ -223,6 +223,7 @@ public class FeedPage extends Page {
 
   @Step
   public int getPostLikeCount(int postIndex) {
+    scrollToElement($$(POST_LOCATOR).get(postIndex));
     return  parseInt($$(POST_LOCATOR)
         .get(postIndex)
         .$(POST_LIKES_COUNTER_LOCATOR)
--- src/test/java/Junit5/ChangeLanguage/ChangeLanguageTest.java
+++ src/test/java/Junit5/ChangeLanguage/ChangeLanguageTest.java
@@ -10,10 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ChangeLanguageTest extends TestBase {
-  private static final String EXPECTED_TOP_NAVIGATION_ITEM_RUSSIAN_TEXT = "МОЯ ЛЕНТА";
-  private static final String EXPECTED_TOP_NAVIGATION_ITEM_GERMAN_TEXT = "NEUIGKEITEN";
-  private static final String EXPECTED_TOP_NAVIGATION_ITEM_FRENCH_TEXT = "MON FLUX";
-  private static final String EXPECTED_TOP_NAVIGATION_ITEM_ENGLISH_TEXT = "MY FEED";
+  String language;
 
   @BeforeAll
   static void openFeed() {
@@ -21,39 +18,242 @@ public class ChangeLanguageTest extends TestBase {
   }
 
   @Story("Change site language")
-  @DisplayName("Check change site language to Russian by not login user")
+  @DisplayName("Check change site language to English by not login user")
   @Test
-  void changeLanguageToRussianTest() {
+  void changeLanguageToEnglishTest() {
+    language = "English";
     header.clickOnChangeLanguageIcon();
-    header.clickOnRussianLanguage();
-    assertEquals(EXPECTED_TOP_NAVIGATION_ITEM_RUSSIAN_TEXT, topNavigation.getFeedTabName());
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Indonesian by not login user")
+  @Test
+  void changeLanguageToIndonesianTest() {
+    language = "Indonesian";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Malay by not login user")
+  @Test
+  void changeLanguageToMalayTest() {
+    language = "Malay";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Catalan by not login user")
+  @Test
+  void changeLanguageToCatalanTest() {
+    language = "Catalan";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Czech by not login user")
+  @Test
+  void changeLanguageToCzechTest() {
+    language = "Czech";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Danish by not login user")
+  @Test
+  void changeLanguageToDanishTest() {
+    language = "Danish";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
   }
 
   @Story("Change site language")
   @DisplayName("Check change site language to German by not login user")
   @Test
   void changeLanguageToGermanTest() {
+    language = "German";
     header.clickOnChangeLanguageIcon();
-    header.clickOnGermanLanguage();
-    assertEquals(EXPECTED_TOP_NAVIGATION_ITEM_GERMAN_TEXT, topNavigation.getFeedTabName());
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Estonian by not login user")
+  @Test
+  void changeLanguageToEstonianTest() {
+    language = "Estonian";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Spanish by not login user")
+  @Test
+  void changeLanguageToSpanishTest() {
+    language = "Spanish";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Filipino by not login user")
+  @Test
+  void changeLanguageToFilipinoTest() {
+    language = "Filipino";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
   }
 
   @Story("Change site language")
   @DisplayName("Check change site language to French by not login user")
   @Test
   void changeLanguageToFrenchTest() {
+    language = "French";
     header.clickOnChangeLanguageIcon();
-    header.clickOnFrenchLanguage();
-    assertEquals(EXPECTED_TOP_NAVIGATION_ITEM_FRENCH_TEXT, topNavigation.getFeedTabName());
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
   }
 
   @Story("Change site language")
-  @DisplayName("Check change site language to English by not login user")
+  @DisplayName("Check change site language to Croatian by not login user")
   @Test
-  void changeLanguageToEnglishTest() {
-    FeedPage.openFeedPage();
+  void changeLanguageToCroatianTest() {
+    language = "Croatian";
     header.clickOnChangeLanguageIcon();
-    header.clickOnEnglishLanguage();
-    assertEquals(EXPECTED_TOP_NAVIGATION_ITEM_ENGLISH_TEXT, topNavigation.getFeedTabName());
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Italian by not login user")
+  @Test
+  void changeLanguageToItalianTest() {
+    language = "Italian";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Hungarian by not login user")
+  @Test
+  void changeLanguageToHungarianTest() {
+    language = "Hungarian";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Polish by not login user")
+  @Test
+  void changeLanguageToPolishTest() {
+    language = "Polish";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Portuguese by not login user")
+  @Test
+  void changeLanguageToPortugueseTest() {
+    language = "Portuguese";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Russian by not login user")
+  @Test
+  void changeLanguageToRussianTestNew() {
+    language = "Russian";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Ukrainian by not login user")
+  @Test
+  void changeLanguageToUkrainianTestNew() {
+    language = "Ukrainian";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Arabic by not login user")
+  @Test
+  void changeLanguageToArabicTestNew() {
+    language = "Arabic";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Hindi by not login user")
+  @Test
+  void changeLanguageToHindiTestNew() {
+    language = "Hindi";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Korean by not login user")
+  @Test
+  void changeLanguageToKoreanTestNew() {
+    language = "Korean";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Japanese by not login user")
+  @Test
+  void changeLanguageToJapaneseTestNew() {
+    language = "Japanese";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Afrikaans by not login user")
+  @Test
+  void changeLanguageToAfrikaansTestNew() {
+    language = "Afrikaans";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
+  }
+
+  @Story("Change site language")
+  @DisplayName("Check change site language to Chinese by not login user")
+  @Test
+  void changeLanguageToChineseTestNew() {
+    language = "Chinese";
+    header.clickOnChangeLanguageIcon();
+    header.chooseLanguage(language);
+    assertEquals(header.expectedLanguage(language), topNavigation.getFeedTabName());
   }
 }
