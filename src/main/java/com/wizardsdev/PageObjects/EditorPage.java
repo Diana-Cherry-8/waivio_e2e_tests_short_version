ef6b43771604f547bb22fdc5e87266f27e88accc
oleksandrmosiichuk <oleksandrmo@wizardsdev.com>
2021-07-14 14:13:15 +0300

    update tests for Advanced report

--- src/main/java/com/wizardsdev/PageObjects/EditorPage.java
+++ src/main/java/com/wizardsdev/PageObjects/EditorPage.java
@@ -1,23 +1,24 @@
 package com.wizardsdev.PageObjects;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import com.wizardsdev.Context;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
 
-public class EditorPage extends Page{
+public class EditorPage extends Page {
     private static final String PAGE_URL = "/editor";
 
     private static final By BUTTON_READY_TO_PUBLISH_LOCATOR = By.cssSelector(".ant-btn-lg");
     private static final By INPUT_TITLE_EDITOR_LOCATOR = By.cssSelector(".md-RichEditor-title");
-    private static final By INPUT_CONTENT_EDITOR_LOCATOR = By.cssSelector(".waiv-editor");
-    private static final By CHECKBOX_LEGAL_NOTICE_IN_PREVIEW_LOCATOR = By.cssSelector(".ant-checkbox-input");
+    private static final By INPUT_CONTENT_EDITOR_LOCATOR = By.cssSelector(".public-DraftStyleDefault-block.public-DraftStyleDefault-ltr");
+    private static final By CHECKBOX_LEGAL_NOTICE_IN_PREVIEW_LOCATOR = By.cssSelector(".ant-checkbox");
     private static final By BUTTON_PUBLISH_IN_PREVIEW_LOCATOR = By.cssSelector(".edit-post__submit-btn");
 
     public EditorPage() {
@@ -33,29 +34,43 @@ public class EditorPage extends Page{
     }
 
     @Step
-    public void setPostTitle(String postTitle){
+    public EditorPage setPostTitle(String postTitle) {
         $(INPUT_TITLE_EDITOR_LOCATOR).shouldBe(Condition.visible).setValue(postTitle);
-    }
-
-
-    @Step
-    public void setContentPost(String contentPost) {
-        //#TODO
+        return this;
     }
 
     @Step
-    public void clickButtonReadyToPublish() {
-        $(BUTTON_READY_TO_PUBLISH_LOCATOR).shouldBe(Condition.visible).click();
+    public EditorPage setContentPost(String contentPost) {
+        $(INPUT_CONTENT_EDITOR_LOCATOR).shouldBe(Condition.visible).sendKeys(contentPost);
+        return this;
     }
 
     @Step
-    public void clickCheckboxLegalNotice() {
+    public EditorPage clickButtonReadyToPublish() {
+        $(BUTTON_READY_TO_PUBLISH_LOCATOR).shouldBe(Condition.enabled).click();
+        return this;
+    }
+
+    @Step
+    public SelenideElement getButtonReadyToPublish() {
+        return $(BUTTON_READY_TO_PUBLISH_LOCATOR);
+    }
+
+    @Step
+    public EditorPage clickCheckboxLegalNotice() {
         $(CHECKBOX_LEGAL_NOTICE_IN_PREVIEW_LOCATOR).shouldBe(Condition.visible).click();
+        return this;
     }
 
     @Step
-    public void clickButtonPublish() {
+    public EditorPage clickButtonPublish() {
         $(BUTTON_PUBLISH_IN_PREVIEW_LOCATOR).shouldBe(Condition.visible).click();
+        return this;
+    }
+
+    @Step
+    public void postAppearWaiter() {
+        sleep(10000);
     }
 
     @Override
--- src/test/java/Junit5/Post/GuestUser/PostCreationTests.java
+++ src/test/java/Junit5/Post/GuestUser/PostCreationTests.java
@@ -1,13 +1,20 @@
 package Junit5.Post.GuestUser;
 
 import Junit5.TestBase;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.wizardsdev.PageObjects.EditorPage;
 import com.wizardsdev.PageObjects.FeedPage;
+import com.wizardsdev.PageObjects.Page;
+import com.wizardsdev.PageObjects.Profile.CommentsPage;
 import com.wizardsdev.PageObjects.Profile.PostsPage;
 import com.wizardsdev.PageObjects.Profile.ProfilePage;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,7 +23,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PostCreationTests extends TestBase {
     String facebookEmail = properties.getProperty("FacebookUserEmail00");
     String facebookPassword = properties.getProperty("FacebookUserPass00");
+    String facebookUserName = properties.getProperty("FacebookUserName00");
     static boolean newWindow = true;
+    String title = String.valueOf((int) (Math.random() * 20));
+    String contentPost = "Test body";
 
 
     @BeforeEach
@@ -25,11 +35,28 @@ public class PostCreationTests extends TestBase {
         header.logInWithFacebook(facebookEmail, facebookPassword, newWindow);
     }
 
-    @Disabled
+    @Story("Editor")
+    @DisplayName("Open editor")
+    @Test
+    void openEditor() {
+        editorPage = EditorPage.openEditor();
+        editorPage.getButtonReadyToPublish().shouldBe(Condition.visible);
+    }
+
     @Story("Post creation")
     @DisplayName("Post creation only with text")
     @Test
     void createPost() {
-        //#TODO
+        openEditor();
+        editorPage
+                .setPostTitle(title)
+                .setContentPost(contentPost)
+                .clickButtonReadyToPublish()
+                .clickCheckboxLegalNotice()
+                .clickButtonPublish()
+                .postAppearWaiter();
+        refreshPage();
+        postsPage = PostsPage.openPostsPage(facebookUserName);
+        assertEquals(title, postsPage.getPostTitle(0));
     }
 }
